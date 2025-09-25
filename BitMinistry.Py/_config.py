0fc83ce7f326b812586b0eed43dcf5e449d433a7
sodi <sodi@hetk.ee>
2025-09-26 02:40:17 +0300

    config remove

--- BitMinistry.Py/_config.py
+++
@@ -1,5 +0,0 @@
-SQLSRV_HOST= "JDEV"
-SQLDB="bmSysLog"
-
-DB_CONN_STR = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+ SQLSRV_HOST +';DATABASE='+ SQLDB +';Trusted_Connection=yes;'
-ALCHEMY_CONN_STR = "mssql+pyodbc://"+SQLSRV_HOST+":1433/"+ SQLDB +"?driver=ODBC+Driver+17+for+SQL+Server"
