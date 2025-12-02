7b312b647c67dee8e0d6afce6779ceb4f0ce8324
andrew <andrew@bitministry.com>
2025-12-02 19:12:34 +0200

    zzz

--- BitMinistry/Exceptions/DenialOfServiceException.cs
+++ BitMinistry/Exceptions/DenialOfServiceException.cs
@@ -4,8 +4,14 @@ namespace BitMinistry.Exceptions
 {
     public class DenialOfServiceException : Exception
     {
-        public DenialOfServiceException( string msg ) :base ( msg )
+        public string Ip { get; }
+        public bool IsLongBan { get; }
+
+        public DenialOfServiceException(string ip, bool isLongBan, string message)
+            : base(message)
         {
+            Ip = ip;
+            IsLongBan = isLongBan;
         }
     }
 }
