eca66e716bca7eb005849117ac511b2fa0dc154e
andrew <andrew@bitministry.com>
2026-05-13 02:00:12 +0300

    qwe

--- BitMinistry/Interfaces/IBSqlCommander.cs
+++ BitMinistry/Interfaces/IBSqlCommander.cs
@@ -13,7 +13,7 @@ namespace BitMinistry
         T LoadById<T>(Expression<Func<T, object>> idCol, object idValue, string[] excludeProps = null) where T : ISqlQueryable;
         IEnumerable<TSqlQueryable> QueryForEntity<TSqlQueryable>(bool reset, string sqlWhereAndOrderBy = null, int? top = null) where TSqlQueryable : ISqlQueryable;
         IEnumerable<TSqlQueryable> QueryForEntity<TSqlQueryable>(Expression<Func<TSqlQueryable, bool>> where, bool reset, int? top = null) where TSqlQueryable : ISqlQueryable;
-        IEnumerable<TSqlQueryable> QueryForSql<TSqlQueryable>(Expression<Func<TSqlQueryable, bool>> where, bool reset, int? top = null) where TSqlQueryable : ISqlQueryable;
+        TSqlQueryable[] QueryForSql<TSqlQueryable>(Expression<Func<TSqlQueryable, bool>> where, bool reset, int? top = null) where TSqlQueryable : ISqlQueryable;
         TSqlQueryable[] QueryForSql<TSqlQueryable>(string query, bool reset) where TSqlQueryable : ISqlQueryable;
         void SetValueFromLocation<TSqlQueryable>(TSqlQueryable obj, Expression<Func<TSqlQueryable, object>> prop, Expression<Func<TSqlQueryable, bool>> where) where TSqlQueryable : ISqlQueryable;
     }
